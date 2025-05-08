c55f77042f921a8731fd953a4c08ad60dcac5c17
Camilo Sierra Lozano <86859620+Cami170299@users.noreply.github.com>
2025-05-08 18:30:51 -0400

    transition

--- app/app.py
+++ app/app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, url_for
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 
 @app.route('/')
 @app.route('/ ') # assigning multiple paths to the same index
